4c9dbfa96af3d4e0e717e3af357f5a91cf4efcc5
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2024-03-29 16:24:19 +0100

    feat: read input files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { MNB } from "./currency/mnb";
+import * as fs from "fs";
+import * as XLSX from "xlsx";
 
-MNB.getExchangeRates(2022).then((response) => console.log(response));
+// MNB.getExchangeRates(2022).then((response) => console.log(response));
+
+const INPUT_PATH = "./files/input/";
+
+fs.readdirSync(INPUT_PATH).forEach((file) => {
+  if (!file.endsWith(".xlsx")) return;
+  const workbook = XLSX.readFile(`${INPUT_PATH}${file}`);
+  console.log(workbook.SheetNames);
+});
