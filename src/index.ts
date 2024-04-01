1f22ea99b63cb2f31a552a6c85eed4cf49f159a6
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2024-04-01 19:53:27 +0200

    feat: convert closed position amounts

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,8 @@ async function handleStatement(s: { name: string; wb: WorkBook }) {
 
   const rates = await MNB.getExchangeRates(oldestDate, newestDate);
 
-  addExchangeRatesToStatement(statement, rates);
+  addExchangeRatesToActivity(statement, rates);
+  addExchangeRatesToClosedPositions(statement, rates);
 
   StatementReader.writeOutputFile(
     statement.getWorkbook(),
@@ -80,7 +81,7 @@ function reformatDate(date: string) {
   return date.split("/").reverse().join("-");
 }
 
-function addExchangeRatesToStatement(statement: Statement, rates: MNBRate[]) {
+function addExchangeRatesToActivity(statement: Statement, rates: MNBRate[]) {
   const activitySheet = statement.sheets["Account Activity"];
   const dateCol = activitySheet.colMap["Date"];
 
@@ -131,6 +132,136 @@ function addExchangeRatesToStatement(statement: Statement, rates: MNBRate[]) {
     };
 
     activitySheet.sheet[`${convertedCol}${i}`] = convertedAmountCell;
+  }
+
+  return statement;
+}
+
+function addExchangeRatesToClosedPositions(
+  statement: Statement,
+  rates: MNBRate[]
+) {
+  const closedPositionsSheet = statement.sheets["Closed Positions"];
+  const openDateCol = closedPositionsSheet.colMap["Open Date"];
+  const closeDateCol = closedPositionsSheet.colMap["Close Date"];
+
+  const exchRateOpenCol = String.fromCharCode(
+    closedPositionsSheet.dimensions.endCol.charCodeAt(0) + 1
+  );
+
+  const convertedOpenCol = String.fromCharCode(
+    closedPositionsSheet.dimensions.endCol.charCodeAt(0) + 2
+  );
+
+  const exchRateCloseCol = String.fromCharCode(
+    closedPositionsSheet.dimensions.endCol.charCodeAt(0) + 3
+  );
+
+  const convertedCloseCol = String.fromCharCode(
+    closedPositionsSheet.dimensions.endCol.charCodeAt(0) + 4
+  );
+
+  const convertedProfitCol = String.fromCharCode(
+    closedPositionsSheet.dimensions.endCol.charCodeAt(0) + 5
+  );
+
+  closedPositionsSheet.sheet[
+    "!ref"
+  ] = `${closedPositionsSheet.dimensions.startCol}${closedPositionsSheet.dimensions.startRow}:${convertedProfitCol}${closedPositionsSheet.dimensions.endRow}`;
+
+  closedPositionsSheet.sheet[
+    `${exchRateOpenCol}${closedPositionsSheet.dimensions.startRow}`
+  ] = {
+    t: "s",
+    v: "Exchange Rate at open date",
+  };
+
+  closedPositionsSheet.sheet[
+    `${exchRateCloseCol}${closedPositionsSheet.dimensions.startRow}`
+  ] = {
+    t: "s",
+    v: "Exchange Rate at close date",
+  };
+
+  closedPositionsSheet.sheet[
+    `${convertedOpenCol}${closedPositionsSheet.dimensions.startRow}`
+  ] = {
+    t: "s",
+    v: "Amount at open (HUF)",
+  };
+
+  closedPositionsSheet.sheet[
+    `${convertedCloseCol}${closedPositionsSheet.dimensions.startRow}`
+  ] = {
+    t: "s",
+    v: "Amount at close (HUF)",
+  };
+
+  closedPositionsSheet.sheet[
+    `${convertedProfitCol}${closedPositionsSheet.dimensions.startRow}`
+  ] = {
+    t: "s",
+    v: "Profit (HUF)",
+  };
+
+  for (
+    let i = closedPositionsSheet.dimensions.startRow + 1;
+    i <= closedPositionsSheet.dimensions.endRow;
+    i++
+  ) {
+    const openDateCell: XLSX.CellObject =
+      closedPositionsSheet.sheet[`${openDateCol}${i}`];
+    if (!openDateCell || !openDateCell.v) {
+      continue;
+    }
+    const openDate = dateAndTimeToDate(openDateCell.v.toString());
+    const openRate = MNB.getExchangeRate(openDate, rates);
+
+    closedPositionsSheet.sheet[`${exchRateOpenCol}${i}`] = {
+      t: "n",
+      v: openRate,
+    };
+
+    const amountCol = closedPositionsSheet.colMap["Amount"];
+
+    const convertedOpenAmountCell: XLSX.CellObject = {
+      t: "n",
+      f: `=${amountCol}${i} * ${exchRateOpenCol}${i}`,
+    };
+
+    closedPositionsSheet.sheet[`${convertedOpenCol}${i}`] =
+      convertedOpenAmountCell;
+
+    const closeDateCell: XLSX.CellObject =
+      closedPositionsSheet.sheet[`${closeDateCol}${i}`];
+    if (!closeDateCell || !closeDateCell.v) {
+      continue;
+    }
+    const closeDate = dateAndTimeToDate(closeDateCell.v.toString());
+    const closeRate = MNB.getExchangeRate(closeDate, rates);
+
+    closedPositionsSheet.sheet[`${exchRateCloseCol}${i}`] = {
+      t: "n",
+      v: closeRate,
+    };
+
+    const profitCol = closedPositionsSheet.colMap["Profit"];
+
+    const convertedCloseAmountCell: XLSX.CellObject = {
+      t: "n",
+      f: `=(${amountCol}${i} - ${profitCol}${i}) * ${exchRateCloseCol}${i}`,
+    };
+
+    closedPositionsSheet.sheet[`${convertedCloseCol}${i}`] =
+      convertedCloseAmountCell;
+
+    const convertedProfitCell: XLSX.CellObject = {
+      t: "n",
+      f: `=${convertedOpenCol}${i} - ${convertedCloseCol}${i}`,
+    };
+
+    closedPositionsSheet.sheet[`${convertedProfitCol}${i}`] =
+      convertedProfitCell;
   }
 
   return statement;
